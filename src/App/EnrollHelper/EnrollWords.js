4d03c91296160e865fc5a52844c6c55c1240cd5e
ligen131 <61000196+ligen131@users.noreply.github.com>
2022-06-21 23:17:48 +0800

    Update EnrollWords.js

--- src/App/EnrollHelper/EnrollWords.js
+++ src/App/EnrollHelper/EnrollWords.js
@@ -30,7 +30,7 @@ export const EnrollReply = [
     reply: [
       {
         type: "text",
-        content: `各位好，我是华科招生辅助机器人。
+        content: `您好！华科招生小机器人为您服务!
 华中科技大学 2022 年在广东省计划共招收 221 人，其中历史类 202 组 27 人，物理类 204 组 153 人，203 组 9 人，207 组 20 人，高校专项 1 人，艺术类 11 人。其中 203 组与 207 组需再选生物或化学。
 去年（2021 年）最各组别最低分数 / 位次为：历史类 202 组 614/1548 ，物理类 204 组 641/4967 ，203 组 653/2585 ，207 组 620/12381 。
 更多招生信息请关注微信公众号：华中科技大学招生办公室。
@@ -38,14 +38,19 @@ export const EnrollReply = [
       },
       {
         type: "url",
-        content: `https://pan.ligen131.com/api/v3/file/get/9/HUST2022%20%E6%8B%9B%E7%94%9F%E8%AE%A1%E5%88%92.jpg?sign=zkPpG3DJUF1QwifvW7554yyvUEyjym4hiPkiZrzAJPw%3D%3A0`,
+        content: `https://sm.ms/image/EYuHrmGtojOfL6x`,
         img_index: 0,
-      },
-      {
-        type: "url",
-        content: `https://pan.ligen131.com/api/v3/file/get/10/HUST2022%20%E6%8A%A5%E8%80%83%E6%8C%87%E5%8D%97.jpg?sign=xV8Gmk3_MOvQ9hTWslD1xV7C6s1R3zFAilBLZ0Yxtio%3D%3A0`,
-        img_index: 1,
       }
+//       {
+//         type: "url",
+//         content: `https://pan.ligen131.com/api/v3/file/get/9/HUST2022%20%E6%8B%9B%E7%94%9F%E8%AE%A1%E5%88%92.jpg?sign=zkPpG3DJUF1QwifvW7554yyvUEyjym4hiPkiZrzAJPw%3D%3A0`,
+//         img_index: 0,
+//       },
+//       {
+//         type: "url",
+//         content: `https://pan.ligen131.com/api/v3/file/get/10/HUST2022%20%E6%8A%A5%E8%80%83%E6%8C%87%E5%8D%97.jpg?sign=xV8Gmk3_MOvQ9hTWslD1xV7C6s1R3zFAilBLZ0Yxtio%3D%3A0`,
+//         img_index: 1,
+//       }
     ],
   },
 ];
