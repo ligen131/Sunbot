ce5e7e88148161094479cf21595558957c105e09
ligen131 <i@ligen131.com>
2023-04-19 22:41:29 +0800

    ♻️refactor(chatgpt): using openai official api

--- src/App/Chatgpt.js
+++ src/App/Chatgpt.js
@@ -1,27 +1,62 @@
 import { Reply } from "../Send.js";
-import { ChatGPTAPI } from 'chatgpt';
+import { ChatGPTClient } from '@waylaidwanderer/chatgpt-api';
+import { KeyvFile } from 'keyv-file';
 import { App } from "./App.js";
 import * as Config from "../config.js";
 
 export { Chatgpt };
 
 class Chatgpt extends App {
-  constructor () {
+  constructor() {
     super(7, "Chatgpt");
     this.InitFunc = this.initApi;
     this.ClockeventFunc = undefined;
     this.ExecuteFunc = this.exec;
 
-    this.api = undefined;
+    this.chatgptClient = undefined;
     this.lastQueryTime = 0;
     this.conversation = new Array();
+    this.chatgptModel = 'gpt-3.5-turbo';
   }
-  
+
   async initApi() {
-    this.api = new ChatGPTAPI({
-      sessionToken: Config.CHATGPT_TOKEN
-    });
-    await this.api.ensureAuth();
+    const clientOptions = {
+      // (Optional) Support for a reverse proxy for the completions endpoint (private API server).
+      // Warning: This will expose your `openaiApiKey` to a third party. Consider the risks before using this.
+      reverseProxyUrl: 'https://openai.labixiaoxing.cn/v1/chat/completions',
+      // (Optional) Parameters as described in https://platform.openai.com/docs/api-reference/completions
+      modelOptions: {
+        // You can override the model name and any other parameters here, like so:
+        model: this.chatgptModel,
+        // I'm overriding the temperature to 0 here for demonstration purposes, but you shouldn't need to override this
+        // for normal usage.
+        temperature: 0,
+        // Set max_tokens here to override the default max_tokens of 1000 for the completion.
+        max_tokens: 1000,
+      },
+      // (Optional) Davinci models have a max context length of 4097 tokens, but you may need to change this for other models.
+      // maxContextTokens: 4097,
+      // (Optional) You might want to lower this to save money if using a paid model like `text-davinci-003`.
+      // Earlier messages will be dropped until the prompt is within the limit.
+      // maxPromptTokens: 3097,
+      // (Optional) Set custom instructions instead of "You are ChatGPT...".
+      // promptPrefix: 'You are Bob, a cowboy in Western times...',
+      // (Optional) Set a custom name for the user
+      // userLabel: 'User',
+      // (Optional) Set a custom name for ChatGPT
+      chatGptLabel: 'Sunbot',
+      // (Optional) Set to true to enable `console.debug()` logging
+      debug: false,
+    };
+
+    const cacheOptions = {
+      // Options for the Keyv cache, see https://www.npmjs.com/package/keyv
+      // This is used for storing conversations, and supports additional drivers (conversations are stored in memory by default)
+      // For example, to use a JSON file (`npm i keyv-file`) as a database:
+      store: new KeyvFile({ filename: 'chatgpt-cache.json' }),
+    };
+
+    this.chatGptClient = new ChatGPTClient(Config.OPENAI_API_KEY, clientOptions, cacheOptions);
     this.LogInfo("[ChatGPT] Completed init ChatGPT API.");
   }
 
@@ -29,28 +64,38 @@ class Chatgpt extends App {
     console.log("Send to ChatGPT: ", text);
     var res = "";
     const now = new Date() - 0;
-    const limit = 20000;
+    const limit = 10000;
     var con;
     if (now - this.lastQueryTime < limit) {
       res = `Requests are too frequent, please try again in ${Math.ceil(limit - (now - this.lastQueryTime))} seconds.`
     }
     else if (text.includes("/reset")) {
-      this.conversation[user] = this.api.getConversation();
+      this.conversation[user] = undefined;
       console.log(user, this.conversation[user]);
       res = `Successfully reset conversation.`;
     } else {
       try {
+        let resp;
         if (!this.conversation[user]) {
-          this.conversation[user] = this.api.getConversation();
+          resp = await this.chatGptClient.sendMessage(text);
+        } else {
+          resp = await 
+            this.chatGptClient.sendMessage(text, {
+              conversationId: this.conversation[user].conversationId,
+              parentMessageId: this.conversation[user].messageId,
+              // If you want streamed responses, you can set the `onProgress` callback to receive the response as it's generated.
+              // You will receive one token at a time, so you will need to concatenate them yourself.
+              // onProgress: token => process.stdout.write(token),
+          })
         }
-        con = this.conversation[user];
         this.lastQueryTime = now;
-        res = await con.sendMessage(text);
+        this.conversation[user] = resp;
+        res = resp.response;
       } catch (err) {
-        res = "Failed to send message to ChatGPT. Please try again later." + err.toString();
+        res = "Failed to send message to ChatGPT. Please try again later. err = " + err.toString();
       }
     }
-    return `[OpenAI ChatGPT]
+    return `[OpenAI ${this.chatgptModel}]
 Q: ${text}
 
 A: ${res}`;
--- src/config-example.js
+++ src/config-example.js
@@ -4,5 +4,5 @@ export const ADMIN_WXID = "wxid_xxxxxxxxxxxx";
 export const WECHATY_PUPPET = 'wechaty-puppet-xp';
 export const WECHATY_PUPPET_TOKEN = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
 export const STATUS_PAGE_PORT = 4720
-export const CHATGPT_TOKEN = '';
+export const OPENAI_API_KEY = '';
 export const SENDING_MESSAGE_ENABLE = true
