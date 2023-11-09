b7ac92dc541e24b90b0ea9badf285650aa87782b
ligen131 <i@ligen131.com>
2023-11-10 00:56:42 +0800

    :sparkles: feat: support gpt-4 chat

--- src/App/Chatgpt.js
+++ src/App/Chatgpt.js
@@ -5,6 +5,7 @@ import { App } from "./App.js";
 import * as Config from "../config.js";
 import { isRoom } from "../Message.js";
 import { rand } from "../Random.js";
+import { Sun_bot } from "../main.js";
 
 export { Chatgpt };
 
@@ -15,10 +16,13 @@ class Chatgpt extends App {
     this.ClockeventFunc = undefined;
     this.ExecuteFunc = this.exec;
 
-    this.chatgptClient = undefined;
+    this.chatgptClient3 = undefined;
+    this.chatgptClient4 = undefined;
     this.lastQueryTime = 0;
-    this.conversation = new Array();
-    this.chatgptModel = 'gpt-3.5-turbo';
+    this.conversation3 = new Array();
+    this.conversation4 = new Array();
+    this.chatgptModel3 = 'gpt-3.5-turbo';
+    this.chatgptModel4 = 'gpt-4-1106-preview';
     this.tips = [
       '发送 /reset 可以重置您与 ChatGPT 的对话，但请注意重置后就无法恢复之前的上下文继续对话了哦。',
       '每次向 ChatGPT 询问都有字数限制，大约 500 个汉字或 1000 个英文单词左右。',
@@ -28,19 +32,20 @@ class Chatgpt extends App {
       'ChatGPT 支持连续对话。如果你觉得他回答有误，可以纠正他。',
       '每个人都有独立的上下文对话，所以在群聊中你是无法继续其他人的上下文和他对话的。',
       '你可以把机器人拉到你想要的群，然后 @他 就可以使用啦。添加机器人好友，私聊他也是可以的。',
-      '如果觉得机器人有用的话，请在 GitHub 上给我一个 Star 吧 https://github.com/ligen131/Sunbot/tree/js-version'
+      '如果觉得机器人有用的话，请在 GitHub 上给我一个 Star 吧 https://github.com/ligen131/Sunbot/tree/js-version', // 8
+      `gpt-4 目前在 Sunbot 上可用！目前使用的模型是 ${this.chatgptModel4}，试试消息开头带上 g4 来与 gpt-4 聊天吧。请注意：gpt-4 的花费是 gpt-3 的 10 倍以上，请不要频繁使用。`
     ];
   }
 
   async initApi() {
-    const clientOptions = {
+    let clientOptions = {
       // (Optional) Support for a reverse proxy for the completions endpoint (private API server).
       // Warning: This will expose your `openaiApiKey` to a third party. Consider the risks before using this.
       reverseProxyUrl: 'https://api.openai.com/v1/chat/completions',
       // (Optional) Parameters as described in https://platform.openai.com/docs/api-reference/completions
       modelOptions: {
         // You can override the model name and any other parameters here, like so:
-        model: this.chatgptModel,
+        model: this.chatgptModel3,
         // I'm overriding the temperature to 0 here for demonstration purposes, but you shouldn't need to override this
         // for normal usage.
         temperature: 1,
@@ -64,51 +69,87 @@ class Chatgpt extends App {
 
     const cacheOptions = {
       // Options for the Keyv cache, see https://www.npmjs.com/package/keyv
-      // This is used for storing conversations, and supports additional drivers (conversations are stored in memory by default)
+      // This is used for storing conversation3s, and supports additional drivers (conversation3s are stored in memory by default)
       // For example, to use a JSON file (`npm i keyv-file`) as a database:
       store: new KeyvFile({ filename: 'chatgpt-cache.json' }),
     };
 
-    this.chatGptClient = new ChatGPTClient(Config.OPENAI_API_KEY, clientOptions, cacheOptions);
+    this.chatGptClient3 = new ChatGPTClient(Config.OPENAI_API_KEY, clientOptions, cacheOptions);
+    clientOptions.modelOptions.model = this.chatgptModel4;
+    this.chatGptClient4 = new ChatGPTClient(Config.OPENAI_API_KEY, clientOptions, cacheOptions);
     this.LogInfo("[ChatGPT] Completed init ChatGPT API.");
   }
 
   async getResponse(text, user) {
-    console.log("Send to ChatGPT: ", text);
     var res = "";
     const now = new Date() - 0;
     const limit = 10000;
-    var con;
     if (now - this.lastQueryTime < limit) {
       res = `请求太过频繁，请稍后再试试吧。`
-    }
+    } else if (text.includes("/reset g4")) {
+      this.conversation4[user] = undefined;
+      console.log(user, this.conversation4[user]);
+      res = `已重置您的 GPT-4 对话。`;
+    } 
     else if (text.includes("/reset")) {
-      this.conversation[user] = undefined;
-      console.log(user, this.conversation[user]);
+      this.conversation3[user] = undefined;
+      console.log(user, this.conversation3[user]);
       res = `已重置您的对话。`;
-    } else {
+    } else if (/^\s*g4/.test(text)) {
+      // gpt-4
       try {
+        text = text.replace(/^\s*g4/, '');
+        console.log("Send to ChatGPT: ", text);
         let resp;
-        if (!this.conversation[user]) {
-          resp = await this.chatGptClient.sendMessage(text);
+        if (!this.conversation4[user]) {
+          resp = await this.chatGptClient4.sendMessage(text);
         } else {
           resp = await 
-            this.chatGptClient.sendMessage(text, {
-              conversationId: this.conversation[user].conversationId,
-              parentMessageId: this.conversation[user].messageId,
+            this.chatGptClient4.sendMessage(text, {
+              conversation4Id: this.conversation4[user].conversation4Id,
+              parentMessageId: this.conversation4[user].messageId,
               // If you want streamed responses, you can set the `onProgress` callback to receive the response as it's generated.
               // You will receive one token at a time, so you will need to concatenate them yourself.
               // onProgress: token => process.stdout.write(token),
           })
         }
         this.lastQueryTime = now;
-        this.conversation[user] = resp;
+        this.conversation4[user] = resp;
         res = resp.response;
       } catch (err) {
         res = "向 ChatGPT 发送信息失败，请稍后再试。错误信息：" + err.toString();
       }
+      return `[OpenAI ${this.chatgptModel4}]
+Q: ${text}
+
+A: ${res}
+
+小提示: ${this.tips[9]}`;
     }
-    return `[OpenAI ${this.chatgptModel}]
+
+    // gpt-3.5-turbo
+    try {
+    console.log("Send to ChatGPT: ", text);
+      let resp;
+      if (!this.conversation3[user]) {
+        resp = await this.chatGptClient3.sendMessage(text);
+      } else {
+        resp = await 
+          this.chatGptClient3.sendMessage(text, {
+            conversation3Id: this.conversation3[user].conversation3Id,
+            parentMessageId: this.conversation3[user].messageId,
+            // If you want streamed responses, you can set the `onProgress` callback to receive the response as it's generated.
+            // You will receive one token at a time, so you will need to concatenate them yourself.
+            // onProgress: token => process.stdout.write(token),
+        })
+      }
+      this.lastQueryTime = now;
+      this.conversation3[user] = resp;
+      res = resp.response;
+    } catch (err) {
+      res = "向 ChatGPT 发送信息失败，请稍后再试。错误信息：" + err.toString();
+    }
+    return `[OpenAI ${this.chatgptModel3}]
 Q: ${text}
 
 A: ${res}
